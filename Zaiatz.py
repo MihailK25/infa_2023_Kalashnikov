0f371068fa9a330bda50b9b2673b744b9ca057fc
MihailK25 <7niki@inbox.ru>
2023-06-01 16:03:39 +0300

    Выполнил Калашников Михаил лаба 5

--- Zaiatz.py
+++ Zaiatz.py
@@ -2,24 +2,27 @@ import pygame
 from pygame.draw import *
 
 pygame.init()
-pygame.display.set_caption('Кролито')  # кастомное название окна
+pygame.display.set_caption('Body Кролитос')  # кастомное название окна
 FPS = 30  # частота кадров
 
 # Цвета
+blue = (80, 208, 255)
 black = (0, 0, 0)
 grey = (184, 184, 184)
 nedogrey = (120, 104, 104)
 bur = (94, 73, 73)
 bodyy = (145, 125, 125)
 white = (255, 255, 255)
+pink = (255,208,160)
+red = (255 ,0 ,0)
 
-# Разрешение окна по вертикали и горизонтали
+# Разрешение окна 800x600
 screen = pygame.display.set_mode((800, 600))
 
-screen.fill((black))  # заливка фона
+screen.fill((blue))  # заливка фона у этого мужчины
 
 f1 = pygame.font.Font(None, 36)
-text1 = f1.render('Заяц', 1, (41, 145, 15))  # цвет шрифта
+text1 = f1.render('Body позитивный Заяц', 1, red)  # цвет шрифта
 
 screen.blit(text1, (10, 50))
 
@@ -91,28 +94,28 @@ def draw_os(surface, x, y, width, height, color):
 
 
 # Отрисовка зайца
-draw_body(screen, 400, 410, 150, 300, bodyy)  # тело кроля
+draw_body(screen, 400, 410, 350, 300, white)  # тело кроля
 
-draw_ear(screen, 360, 100, 40, 200, nedogrey)  # левое ухо
-draw_ear(screen, 440, 100, 40, 200, nedogrey)  # правое ухо
+draw_ear(screen, 360, 100, 60, 140, pink)  # левое ухо
+draw_ear(screen, 440, 100, 60, 140, pink)  # правое ухо
 
-draw_head(screen, 400, 200, 150, grey)  # голова кроля
+draw_head(screen, 400, 200, 180, white)  # голова кроля
 
-draw_leg(screen, 280, 320, 120, 40, bur)  # левая верхняя лапа
-draw_leg(screen, 520, 320, 120, 40, bur)  # правая верхняя лапа
+draw_leg(screen, 240, 340, 80, 50, pink)  # левая верхняя лапа
+draw_leg(screen, 560, 340, 80, 50, pink)  # правая верхняя лапа
 
-draw_leg(screen, 317, 550, 100, 50, bur)  # левая нижняя лапа
-draw_leg(screen, 485, 550, 100, 50, bur)  # правая нижняя лапа
+draw_leg(screen, 317, 550, 100, 80, pink)  # левая нижняя лапа
+draw_leg(screen, 485, 550, 100, 80, pink)  # правая нижняя лапа
 
-draw_eye(screen, 360, 180, 27, 27, white)  # правый белок глаза
-draw_eye(screen, 363, 180, 7, 7, black)  # правый зрачок
-draw_eye(screen, 436, 180, 27, 27, white)  # левый белок глаза
-draw_eye(screen, 438, 180, 7, 7, black)  # левый зрачок
+draw_eye(screen, 363, 180, 15, 10, black)  # правый зрачок
+draw_eye(screen, 438, 180, 15, 10, black)  # левый зрачок
 
-draw_os(screen, 400, 230, 70, 40, black)  # рот
+draw_os(screen, 400, 230, 40, 40, grey)  # рот
 
-draw_ear(screen, 397, 218, 5, 15, white)  # левый зуб
-draw_ear(screen, 403, 218, 5, 15, white)  # правый зуб
+draw_ear(screen, 396, 218, 7, 15, white)  # верхний левый зуб
+draw_ear(screen, 405, 218, 7, 15, white)  # верхний правый зуб
+draw_ear(screen, 405, 242, 7, 15, white)  # нижний левый зуб
+draw_ear(screen, 396, 242, 7, 15, white)  # нижний правый зуб
 
 pygame.display.update()
 clock = pygame.time.Clock()
